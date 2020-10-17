bdc23de765f0ff7dac4cead4ab2887c2c593108c
Brendan O'Connell <bmoconnell15@gmail.com>
2020-10-17 22:06:37 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ end
 
 def get_english_meaning(file_path, emoticon)
   hash = load_library(file_path)
-    dict = hash.select {|key,value| value[:japanese] == emoticon}
-    dict.keys[0]
+  dict = hash.select {|key,value| value[:japanese] == emoticon}
+  dict.keys[0]
   end 
 end
